5d18fcf88e88ca00e9cb3a2d9862d0493f1e17d0
Chettipellysrilatha <srilathachettipelly2@gmail.com>
2026-02-03 18:41:33 +0530

    Configure s3 backend for terraform state

--- provider.tf
+++ provider.tf
@@ -1,14 +1,9 @@
 terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+  backend "s3" {
+    bucket         = "terraform-state-srilatha-001"
+    key            = "alb-ec2-docker/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
-}
-
-provider "aws" {
-  region = "ap-south-1"
 }
